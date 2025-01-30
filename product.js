cc6eac47444bb788d6c37a65b52e48e96ddb1c49
victoriawerngreen <viggomussen@gmail.com>
2025-01-30 19:02:43 +0100

    add ${data.___}

--- product.js
+++ product.js
@@ -1,23 +1,33 @@
-let productContainer=document.querySelector(".product_1-1")
+const productContainer = document.querySelector(".product_1-1");
+const productId = 1525;
 
-fetch("https://kea-alt-del.dk/t7/images/webp/640/1573.webp")
+fetch(`https://kea-alt-del.dk/t7/api/products/${productId}`)
 .then(responce => responce.json())
 // skriver () efter .json fordi det er en funktion der allerede findes og vi kalder på den via ()
 .then(data => {
     productContainer.innerHTML = `
-          <img src="1525_bag.webp" alt="produkt billede" />
-        </div>
+           <img
+          src="https://kea-alt-del.dk/t7/images/webp/640/${data.id}.webp"
+          alt="Produktbillede"
+        />
         <div class="product_margin">
-          <h2 class="brand-category">PUMA | Sporting goods</h2>
-          <h1>Deck Navy Blue Backpack</h1>
-          <p class="description">Backpack</p>
-          <p class="price">1299,95DKK</p>
-          <p class="rabat">55% off</p>
-          <p class="price-m-rabat">Now 584,96DKK</p>
+          <h2 class="brand-category">${data.brandname} | ${data.category}</h2>
+
+          <h1>${data.productdisplayname}</h1>
+
+          <p class="description">${data.articletype} </p>
+
+          <p class="price">${data.price}DKK</p>
+
+          <p class="rabat">${data.discount}% off</p>
+
+          <p class="price-m-rabat">Now 584DKK</p>
+
           <div class="grid_1-1">
             <p class="option">Color:</p>
             <p>Navy</p>
           </div>
+
           <div class="grid_1-1">
             <p class="option">Size:</p>
             <p>One-size</p>
@@ -25,5 +35,8 @@ fetch("https://kea-alt-del.dk/t7/images/webp/640/1573.webp")
 
           <div class="laeg-i-kurv">
             <p class="kurv_knap">Add to basket</p>
-          </div>`;
-})
+          </div>
+          `;
+});
+
+
