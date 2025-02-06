d99caedc879c321f6c9eb5d724bf9103cd6f6240
victoriawerngreen <viggomussen@gmail.com>
2025-02-06 11:24:03 +0100

    fix subgrid

--- productlist.js
+++ productlist.js
@@ -15,22 +15,15 @@ function showList(products) {
   const markup = products
     .map(
       (product) =>
-        `<div class="product-item"  ">
-          
+        `<div class="product-item">
         <p class="sold_out ${product.soldout && "show"}">Sold Out</p>
-      <a href="product.html?productId=${product.id}">
-      <img src="https://kea-alt-del.dk/t7/images/webp/640/${product.id}.webp"
-          alt="Produktbillede"/></a>
-          
-          
-          <div class="info">
-            <p class="produkt_titel">${product.productdisplayname}</p>
-            <p>${product.brandname} | ${product.subcategory}</p>
-            <p class="price_view">${product.price}DKK</p>
-            <p class="rabat ${!product.discount && "hide"}">${product.discount}% off</p>
-            <p class="rabat_price ${!product.discount && "hide"}">Now ${Math.round(product.price - (product.price / 100) * product.discount)} DKK</p>
-
-          </div>
+        <a href="product.html?productId=${product.id}">
+        <img src="https://kea-alt-del.dk/t7/images/webp/640/${product.id}.webp"alt="Produktbillede"/></a>
+        <p class="produkt_titel">${product.productdisplayname}</p>
+        <p>${product.brandname} | ${product.subcategory}</p>
+        <p class="price_view">${product.price}DKK</p>
+        <p class="rabat ${!product.discount && "hide"}">${product.discount}% off</p>
+        <p class="rabat_price ${!product.discount && "hide"}">Now ${Math.round(product.price - (product.price / 100) * product.discount)} DKK</p>
         </div>`
     )
     .join("");
