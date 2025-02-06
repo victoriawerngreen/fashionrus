3c62b85d5457da1eeeabc222393b7804111ae7db
victoriawerngreen <viggomussen@gmail.com>
2025-02-06 08:34:15 +0100

    add/fix souldout & rabat

--- productlist.js
+++ productlist.js
@@ -4,7 +4,7 @@ const overskrift = document.querySelector("h1");
 
 overskrift.innerHTML = myCategory;
 
-fetch(`https://kea-alt-del.dk/t7/api/products?category=${myCategory}`)
+fetch(`https://kea-alt-del.dk/t7/api/products?category=${myCategory}&limit=100`)
   .then((response) => response.json())
   // skriver () efter .json fordi det er en funktion der allerede findes og vi kalder på den via ()
   .then((data) => showList(data));
@@ -15,14 +15,19 @@ function showList(products) {
   const markup = products
     .map(
       (product) =>
-        `<div class="product-item">
-          <a href="product.html?productId=${product.id}"><img
-          src="https://kea-alt-del.dk/t7/images/webp/640/${product.id}.webp"
+        `<div class="product-item"  ">
+          <div class="image-wrapper">
+        <p class="sold_out ${product.soldout && "soldOut"}">Sold Out</p>
+      <a href="product.html?productId=${product.id}">
+      <img src="https://kea-alt-del.dk/t7/images/webp/640/${product.id}.webp"
           alt="Produktbillede"/></a>
+          </div>
+          
           <div class="info">
             <p class="produkt_titel">${product.productdisplayname}</p>
             <p>${product.brandname} | Sporting goods</p>
             <p class="price_view">${product.price}DKK</p>
+            <p class="rabat ${product.discount && "isOnSale"}">${product.discount}% off</p>
           </div>
         </div>`
     )
